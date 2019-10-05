be0201169480d2590c2fd275abac17de696df630
Nia Ibrahim <nibrahi3@uncc.edu>
2019-10-04 23:54:29 -0400

    New source class.

--- app/src/main/java/com/example/hw05/MainActivity.java
+++ app/src/main/java/com/example/hw05/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.hw05;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        if(isConnected()){
+            
+        }else{
+            Toast.makeText(this, "No Internet Connection!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private boolean isConnected() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        if (networkInfo == null || !networkInfo.isConnected() ||
+                (networkInfo.getType() != ConnectivityManager.TYPE_WIFI
+                        && networkInfo.getType() != ConnectivityManager.TYPE_MOBILE)) {
+            return false;
+        }
+        return true;
     }
 }
---
+++ app/src/main/java/com/example/hw05/Source.java
@@ -0,0 +1,7 @@
+package com.example.hw05;
+
+public class Source {
+
+    public int id;
+    public String name;
+}
